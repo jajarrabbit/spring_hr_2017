874efe949deca480abc8cca1bed9ff0a84299b7b
Kwanchanok <kwanchanok@arms-asia.com>
2017-01-12 10:21:15 +0700

    first commit structure.

---
+++ src/main/java/com/arms/SpringHr2017Application.java
@@ -0,0 +1,12 @@
+package com.arms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHr2017Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHr2017Application.class, args);
+	}
+}
